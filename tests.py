aee5ab273c3eac65ab47185299ec22e7bf8a5dab
Ebubechukwu Nwamaka <76922403+saybube@users.noreply.github.com>
2021-05-24 20:48:52 +0100

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+
+# Create your tests here.
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='just a test')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
+
