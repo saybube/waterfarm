668c5f407e9a39aa54ef1cf0e7979bec77d9415b
Ebube <you@example.com>
2021-05-30 20:06:02 +0100

    A commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
 from .models import Post
 
 
@@ -17,3 +16,20 @@ class PostModelTest(TestCase):
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
 
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
